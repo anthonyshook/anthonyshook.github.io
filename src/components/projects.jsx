cecbcdb9293e238178d56d91560c9c60eb0c96ff
Anthony Shook <anthonyshook@gmail.com>
2020-08-11 01:45:53 -0500

    Project updates

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import data from "./cv.json"; 
+
+const projects = data.Projects;
 
 export default class Projects extends Component {
     render() {
@@ -8,7 +11,19 @@ export default class Projects extends Component {
                     <div class='inner-container'>
                         <div class='title'>Projects</div>
                         <div class='title-divider'></div>
-                        Coming Soon
+                        <div id="projects-container">
+                            {projects.map(project => (
+                                <div class='project-card'>
+                                    <div class="project-title">{project.Name}</div>
+                                    <div class="project-desc">{project.Desc}</div>
+                                    {project.Skills.map(skill => (
+                                        <div class="project-skills"><p>{skill}</p></div>
+                                    ))}
+                                    <div class="project-link"><a href={project.Link}>View Project</a></div>
+                                </div>
+                            ))}
+                        </div>
+                        More to come...
                     </div>
                 </section>
             </div>
