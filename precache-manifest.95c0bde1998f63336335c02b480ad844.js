03244295379d8b83ed57621177e561f55b714e7f
root <root@DESKTOP-KNV63RU.localdomain>
2020-08-10 20:26:50 -0500

    Updates

---
+++ precache-manifest.95c0bde1998f63336335c02b480ad844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bfd9f1f3d2818eb48d8628b9bbd30c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d025e9d81cd8897037a3",
+    "url": "/static/css/main.02256952.chunk.css"
+  },
+  {
+    "revision": "e0b009f087aa114e144f",
+    "url": "/static/js/2.15dc3f24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15dc3f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d025e9d81cd8897037a3",
+    "url": "/static/js/main.2086ebbf.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  }
+]);
